8f76661ec334886d7152168188d462c3a1ea04c7
cater20 <onyangoketta@gmail.com>
2026-02-18 13:36:28 +0300

    main updated DB storage intialized

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/yourname/go-todo-api/routes"
+	
+	"github.com/cater20/go-todo-api/routes"
+	"github.com/cater20/go-todo-api/storage"
 )
 
 func main() {
+
+	storage.InitDB() // initialize SQLite
+	
 	router := routes.SetupRoutes() // Get router from routes package
 	fmt.Println("Server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
