d6159573fb165ec91921212f26e609d008a7ac23
cater20 <onyangoketta@gmail.com>
2026-02-19 16:46:04 +0300

    deleted:    frontend/app.js         deleted:    frontend/index.html         deleted:    frontend/style.css         modified:   main.go          created:       web/

--- main.go
+++ main.go
@@ -5,16 +5,27 @@ import (
 	"log"
 	"net/http"
 
-	
 	"github.com/cater20/go-todo-api/routes"
 	"github.com/cater20/go-todo-api/storage"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
+	// Initialize SQLite DB
+	storage.InitDB()
 
-	storage.InitDB() // initialize SQLite
-	
-	router := routes.SetupRoutes() // Get router from routes package
+	// Create main router
+	router := mux.NewRouter()
+
+	// Setup API routes
+	apiRouter := routes.SetupRoutes()        // returns *mux.Router
+	router.PathPrefix("/todos").Handler(apiRouter)
+
+	// Serve frontend static files
+	fs := http.FileServer(http.Dir("./frontend"))
+	router.PathPrefix("/").Handler(fs)
+
 	fmt.Println("Server running on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
